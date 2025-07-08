12f02dc5622c30b5126bcf3180a95a1db1a301a3
Aybek <aybek.djumashev@gmail.com>
2025-07-08 17:33:26 +0500

    .

--- testapp/tasks.py
+++ testapp/tasks.py
@@ -40,9 +40,8 @@ def send_telegram(tests):
             keyboards.append(keyboard)
         elif len(test_names) == 1:
             sorted_tests = sorted(tests, key=lambda x: x.subject.grade) 
-            message = f"<b>📌 {list(test_names)[0]} páninen test.</b>\n\n" 
-            message += f"🎒 {','.join([str(test.subject.grade) for test in sorted_tests])}-klasslar ushın\n"
-            message += f'⏰ Baslanıw waqıtı: {tests[0].subject.start_time.strftime("%H:%M")}\n\n'
+            message = f"<b>📌 {list(test_names)[0]} páninen test.</b>\n" 
+            message += f"🎒 {','.join([str(test.subject.grade) for test in sorted_tests])}-klasslar ushın\n\n"
             message += f"@bilimler_bellesiwi | #{list(test_names)[0].lower().replace(' ', '_')}_test"
             # sorted_tests = sorted(tests, key=lambda x: x.subject.grade) 
             # row_keyboards = []
